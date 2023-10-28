54274fb0b02cba7c0bdd78f565c55b0c97a99fa1
manik712 <ranineela71@gmail.com>
2023-10-29 00:00:53 +0600

    added

--- src/pagese/Home/baner/Banner.jsx
+++ src/pagese/Home/baner/Banner.jsx
@@ -10,7 +10,18 @@ const Banner = () => {
   return (
     <div className="carousel w-full h-[600px]">
   <div id="slide1" className="carousel-item relative w-full">
-    <img src={img1} className="w-full" />
+    <img src={img1} className="w-full rounded-xl" />
+    <div className="absolute flex h-full left-0 top-0 items-center  bottom-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
+      <div className="text-white space-y-7 w-1/3 ml-12">
+      <h3 className="text-6xl font-bold">Affordable Price For Car Servicing</h3>
+      <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+      <div className="">
+      <button className="btn btn-warning rounded-lg mr-5 ">Discover More</button>
+      <button className="btn btn-outline btn-warning ">Latest Project</button>
+      </div>
+      
+      </div>
+    </div>
     <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
       <a href="#slide6" className="btn btn-circle mr-4">❮</a> 
       <a href="#slide2" className="btn btn-circle">❯</a>

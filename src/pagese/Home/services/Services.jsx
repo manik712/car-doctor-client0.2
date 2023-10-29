195fde09f8d87068e5702985748f191464d1d523
manik712 <ranineela71@gmail.com>
2023-10-29 13:54:18 +0600

    services.json

---
+++ src/pagese/Home/services/Services.jsx
@@ -0,0 +1,16 @@
+
+
+const Services = () => {
+  return (
+    <div >
+        <div className="text-center space-y-6 mt-5">
+           <h3 className="text-xl text-red-500 font-extrabold">Service</h3>
+           <h2 className="text-5xl font-bold">Our Service Area</h2>
+           <p className="text-slate-500 font-semibold">the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which do not look even slightly believable. </p>
+
+        </div>
+    </div>
+  );
+};
+
+export default Services;

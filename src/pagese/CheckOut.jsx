c3ac578fe9f11d0b1612e5dacbb5940990605c7c
manik712 <ranineela71@gmail.com>
2023-11-12 19:31:34 +0600

    ok

--- src/pagese/CheckOut.jsx
+++ src/pagese/CheckOut.jsx
@@ -4,67 +4,89 @@ import { AuthContext } from "../providers/AuthProviders";
 
 function CheckOut() {
   const service = useLoaderData();
-  const { title, price } = service;
-  const {user }= useContext(AuthContext)
-  const handleBookService = (event)=>{
-
- 
-   event.preventDefault();
-   const form = event.target;
-  //  const name = form.name
-
-
-
-
-
-
-
-
-
-
-
-  }
-  return <div>
-
-
-    <h3 className="text-center text-3xl font-bold">Book service:{title}</h3>
+  const { title, price, _id } = service;
+  const { user } = useContext(AuthContext);
+  const handleBookService = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const date = form.date.value;
+    const email = user?.email;
+    const dueAmount = form.dueAmount.value;
+    const order = {
+      name,
+      date,
+      email,
+      dueAmount,
+      _id,
+    };
+    console.log(order);
+  };
+  return (
+    <div>
+      <h3 className="text-center text-3xl font-bold">Book service:{title}</h3>
 
       <form onSubmit={handleBookService}>
-       
-       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-       <div className="form-control">
-          <label className="label">
-            <span className="label-text">Name</span>
-          </label>
-          <input type="email" name="name" className="input input-bordered" required />
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="name"
+              name="name"
+              defaultValue={user?.displayName}
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Date</span>
+            </label>
+            <input
+              type="date"
+              name="date"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              name="email"
+              placeholder="email"
+              defaultValue={user?.email}
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Due amount</span>
+            </label>
+            <input
+              type="text"
+              defaultValue={"$" + price}
+              name="dueAmount"
+              className="input input-bordered"
+              required
+            />
+          </div>
         </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Date</span>
-          </label>
-          <input type="date" name="date" className="input input-bordered" required />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="email" name="email" placeholder="email" defaultValue={user?.email} className="input input-bordered" required />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Due amount</span>
-          </label>
-          <input type="text"  defaultValue={'$'+price} className="input input-bordered" required />
-          
-        </div>
-       </div>
         <div className="form-control mt-6">
-          <button >Login</button>
-          <input    className="btn btn-primary btn-block"   type="submit" placeholder="Order Confirm" />
+          <input
+            className="btn btn-primary btn-block"
+            type="submit"
+            placeholder="Order Confirm"
+          />
         </div>
       </form>
     </div>
- 
+  );
 }
 
 export default CheckOut;
